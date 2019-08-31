20d7f4047da3551f3bc006ff59ce36ad114264b6
Rex <talebook@foxmail.com>
2019-09-01 00:24:52 +0800

    书籍导航页面增加跳转功能

--- vue-calibre/src/pages/BookNav.vue
+++ vue-calibre/src/pages/BookNav.vue
@@ -3,7 +3,7 @@
         <template v-for="nav in navs">
         <v-flex :key="nav.legend">
             <h2>{{nav.legend}}</h2>
-            <v-chip v-for="item in nav.tags" :key="item.name" outline :color="item.count != 0 ? 'primary': 'default'" >
+            <v-chip v-for="item in nav.tags" @click="$router.push('/tag/'+item.name)" :key="item.name" outline :color="item.count != 0 ? 'primary': 'default'" >
                 {{item.name}}
                 <span v-if="item.count">&nbsp;({{item.count}})</span>
             </v-chip>
