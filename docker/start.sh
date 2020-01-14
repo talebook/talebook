ea42c4a35c6147aa540a5747668fa5565633d481
Rex <talebook@foxmail.com>
2020-01-14 12:01:00 +0800

    try fix bug: upgrade from old-version

--- docker/start.sh
+++ docker/start.sh
@@ -4,6 +4,14 @@ if [ ! -d "/data/books" ]; then
   cp -rf /prebuilt/books /data/
 fi
 
+if [ ! -d "/data/books/settings" ]; then
+  cp -rf /prebuilt/books/settings /data/books/
+fi
+
+if [ ! -s "/data/books/calibre-webserver.db" ]; then
+  cp /prebuilt/books/calibre-webserver.db /data/books/
+fi
+
 if [ ! -d "/data/log" ]; then
   cp -rf /prebuilt/log /data/
 fi
