d52dc0a47de62c3cee3fd6baa62907f7beb81f01
Rex <talebook@foxmail.com>
2022-02-22 00:46:40 +0800

    google字体和mdi图标本地化

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -14,17 +14,23 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' },
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+      //{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' },
+      { hid: 'description', name: 'description', content: '这是个安静读书的地方。在线阅读Epub/Mobi/Pdf/Azw3等格式的电子书，也支持下载或推送到Kindle设备里。' },
+      { name: 'keywords', content: '在线阅读 电子书 下载 推送 kindle epub mobi' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+       'material-design-icons-iconfont/dist/material-design-icons.css',
+       '@mdi/font/css/materialdesignicons.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -39,6 +45,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/vuetify',
+    '@nuxtjs/google-fonts',
+    '@nuxtjs/google-analytics',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -47,29 +55,26 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    iconfont: 'mdi',
-    theme: {
-      dark: false,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      defaultAssets: false,
+      iconfont: 'mdi',
+  },
+
+  googleFonts: {
+      download: true
+    },
+
+  /*
+  googleAnalytics: {
+    // Use as fallback if no runtime config is provided
+    id: process.env.GOOGLE_ANALYTICS_ID || "UA-45286404-2",
+  },
+  */
+
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS_ID
     }
   },
-
-  server: {
-    port: 8081, // default: 3000
-    host: '0.0.0.0' // default: localhost
-  },
-
 
   privateRuntimeConfig: {
     baseURL: process.env.BASE_URL,
@@ -78,6 +83,11 @@ export default {
   serverMiddleware: [
     logger({ format: "%date %status %method %url (%time)" })
   ],
+
+  server: {
+    port: 8081, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
