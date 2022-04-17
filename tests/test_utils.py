96d28ecc18eef57ce0bf1561df93930a843a75f4
Rex <talebook@foxmail.com>
2022-04-17 16:01:41 +0800

    增加个单元测试

---
+++ tests/test_utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pytest
+# -*- coding: UTF-8 -*-
+
+
+import unittest
+
+from webserver.handlers.meta import compare_books
+
+
+class TestUtils(unittest.TestCase):
+    def test_compare_by_rating_or_id(self):
+        cases = [
+            [1, {"id": 2}, {"id": 1}],
+            [1, {"rating": 2}, {"rating": None}],
+            [1, {"rating": 2}, {"rating": 0}],
+            [1, {"rating": 2}, {}],
+            [1, {"rating": 2}, {"id": 2}],
+            [1, {"rating": 2, "id": 2}, {"rating": 2, "id": 1}],
+            [1, {"rating": 0, "id": 2}, {"rating": 0, "id": 1}],
+            [1, {"rating": 0, "id": 2}, {"rating": None, "id": 1}],
+            [1, {"rating": 0, "id": 2}, {"id": 1}],
+        ]
+        for val, a, b in cases:
+            self.assertEqual(val, compare_books(a, b), "compare %s > %s" % (a, b))
+            self.assertEqual(-1 * val, compare_books(b, a), "compare %s > %s" % (b, a))
