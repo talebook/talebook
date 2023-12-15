33c1a6023b5c41b86f47331e126b8e29cccc27f4
shenyelaile <116306402+shenyelaile@users.noreply.github.com>
2023-12-15 15:01:52 +0800

    Update AppFooter.vue
    删除最下行链接

--- app/src/components/AppFooter.vue
+++ app/src/components/AppFooter.vue
@@ -3,12 +3,6 @@
         <v-col cols=12 class='text-center'>
             <v-divider class='mt-10 mb-3' ></v-divider>
             <p class='mb-0 text-center footer-text' v-html="footer_text"></p>
-            <p>
-            <v-btn small text target="_blank" href="https://github.com/talebook/talebook">Github</v-btn>
-            | <v-btn small text target="_blank" href="https://hub.docker.com/r/talebook/talebook">Docker</v-btn>
-            | <v-btn small text target="_blank" href="https://github.com/talebook/talebook-library">Library</v-btn>
-            | <v-btn small text target="_blank" href="http://talebook.org">Project</v-btn>
-            </p>
         </v-col>
     </v-row>
 </template>
