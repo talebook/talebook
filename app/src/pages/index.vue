3503151ff556cdfc48952eb14bf4af1b28a010d0
shenyelaile <116306402+shenyelaile@users.noreply.github.com>
2023-12-15 15:24:13 +0800

    Update index.vue

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -2,7 +2,7 @@
     <div>
     <v-row>
         <v-col cols=12>
-            <p class="ma-0 title">随便推荐</p>
+            <p class="ma-0 title">随机推荐</p>
         </v-col>
         <v-col cols=6 xs=6 sm=4 md=2 lg=1 v-for="(book,idx) in get_random_books" :key="'rec'+idx+book.id" class="book-card">
             <v-card :to="book.href" class="ma-1">
