324f0e0d41db02b8b2549c3d59b3463f6b7cc459
Rex <talebook@foxmail.com>
2024-10-23 19:07:59 +0800

    feat: 删掉无效链接

--- app/src/components/AppFooter.vue
+++ app/src/components/AppFooter.vue
@@ -6,7 +6,6 @@
             <p>
             <v-btn small text target="_blank" href="https://github.com/talebook/talebook">Github</v-btn>
             | <v-btn small text target="_blank" href="https://hub.docker.com/r/talebook/talebook">Docker</v-btn>
-            | <v-btn small text target="_blank" href="https://github.com/talebook/talebook-library">Library</v-btn>
             | <v-btn small text target="_blank" href="http://talebook.org">Project</v-btn>
             </p>
         </v-col>
