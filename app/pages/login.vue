70c69e5adf7bd15cff3af7ab86effa3a8f65b781
hehetoshang <shangzhen831@163.com>
2026-02-06 17:45:11 +0800

    feat(登录注册页): 统一登录和注册页面的UI布局
    - 移除原有的空白布局，使用与首页相同的顶部栏和侧边栏组件
    - 调整页面结构和样式，保持整体UI一致性
    - 添加导航栏显示控制开关，便于后续功能扩展
    - 更新相关文档和任务清单

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,160 +1,162 @@
 
 <template>
-    <v-row
-        justify="center"
-        class="fill-center"
-    >
-        <v-col
-            cols="12"
-            sm="8"
-            md="4"
+    <div class="login-container">
+        <v-row
+            justify="center"
+            class="mt-8"
         >
-            <v-card
-                v-if="show_login"
-                class="elevation-12"
+            <v-col
+                cols="12"
+                sm="8"
+                md="4"
             >
-                <v-toolbar
-                    dark
-                    color="primary"
+                <v-card
+                    v-if="show_login"
+                    class="elevation-12"
                 >
-                    <v-toolbar-title>欢迎访问</v-toolbar-title>
-                    <v-spacer />
-                    <v-btn
-                        v-if="allowRegister"
-                        rounded
-                        color="success"
-                        variant="elevated"
-                        to="/signup"
-                        class="text-white mr-4"
+                    <v-toolbar
+                        dark
+                        color="primary"
                     >
-                        注册
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text>
-                    <v-form @submit.prevent="do_login">
-                        <v-text-field
-                            v-model="username"
-                            prepend-icon="mdi-account"
-                            label="用户名"
-                            type="text"
-                        />
-                        <v-text-field
-                            id="password"
-                            v-model="password"
-                            prepend-icon="mdi-lock"
-                            label="密码"
-                            type="password"
-                        />
-                        <p class="text-right">
-                            <a
-                                class="press-content"
-                                href="javascript:void(0)"
-                                @click="show_login = !show_login"
-                            > 忘记密码? </a>
-                        </p>
+                        <v-toolbar-title>欢迎访问</v-toolbar-title>
+                        <v-spacer />
+                        <v-btn
+                            v-if="allowRegister"
+                            rounded
+                            color="success"
+                            variant="elevated"
+                            to="/signup"
+                            class="text-white mr-4"
+                        >
+                            注册
+                        </v-btn>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form @submit.prevent="do_login">
+                            <v-text-field
+                                v-model="username"
+                                prepend-icon="mdi-account"
+                                label="用户名"
+                                type="text"
+                            />
+                            <v-text-field
+                                id="password"
+                                v-model="password"
+                                prepend-icon="mdi-lock"
+                                label="密码"
+                                type="password"
+                            />
+                            <p class="text-right">
+                                <a
+                                    class="press-content"
+                                    href="javascript:void(0)"
+                                    @click="show_login = !show_login"
+                                > 忘记密码? </a>
+                            </p>
+                            <div align="center">
+                                <v-btn
+                                    type="submit"
+                                    large
+                                    rounded
+                                    color="primary"
+                                    :loading="loading"
+                                >
+                                    登录
+                                </v-btn>
+                            </div>
+                        </v-form>
+                    </v-card-text>
+
+                    <v-card-text v-if="socials && socials.length > 0">
+                        <v-divider />
                         <div align="center">
-                            <v-btn
-                                type="submit"
-                                large
-                                rounded
-                                color="primary"
-                                :loading="loading"
+                            <br />
+                            <small>使用社交网络账号登录</small>
+                            <br />
+                            <template
+                                v-for="s in socials"
+                                :key="s.text"
                             >
-                                登录
-                            </v-btn>
+                                <v-btn
+                                    small
+                                    outlined
+                                    :href="'/auth/login/' + s.value"
+                                    class="ma-1"
+                                >
+                                    {{ s.text }}
+                                </v-btn>
+                            </template>
                         </div>
-                    </v-form>
-                </v-card-text>
+                    </v-card-text>
+                    <v-alert
+                        v-if="alert.msg"
+                        :type="alert.type"
+                        class="ma-2"
+                    >
+                        {{ alert.msg }}
+                    </v-alert>
+                </v-card>
 
-                <v-card-text v-if="socials && socials.length > 0">
-                    <v-divider />
-                    <div align="center">
-                        <br>
-                        <small>使用社交网络账号登录</small>
-                        <br>
-                        <template
-                            v-for="s in socials"
-                            :key="s.text"
-                        >
-                            <v-btn
-                                small
-                                outlined
-                                :href="'/auth/login/' + s.value"
-                                class="ma-1"
-                            >
-                                {{ s.text }}
-                            </v-btn>
-                        </template>
-                    </div>
-                </v-card-text>
-                <v-alert
-                    v-if="alert.msg"
-                    :type="alert.type"
-                    class="ma-2"
+                <v-card
+                    v-else
+                    class="elevation-12"
                 >
-                    {{ alert.msg }}
-                </v-alert>
-            </v-card>
-
-            <v-card
-                v-else
-                class="elevation-12"
-            >
-                <v-toolbar
-                    dark
-                    color="red"
-                >
-                    <v-toolbar-title>重置密码</v-toolbar-title>
-                </v-toolbar>
-                <v-card-text>
-                    <v-form @submit.prevent="do_reset">
-                        <v-text-field
-                            v-model="username"
-                            prepend-icon="mdi-account"
-                            label="用户名"
-                            type="text"
-                        />
-                        <v-text-field
-                            v-model="email"
-                            prepend-icon="mdi-email"
-                            label="注册邮箱"
-                            type="text"
-                            autocomplete="old-email"
-                        />
-                        <div
-                            align="center"
-                            class="mt-4"
-                        >
-                            <v-btn
-                                rounded
-                                color=""
-                                class="mr-5"
-                                @click="show_login = !show_login"
+                    <v-toolbar
+                        dark
+                        color="red"
+                    >
+                        <v-toolbar-title>重置密码</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form @submit.prevent="do_reset">
+                            <v-text-field
+                                v-model="username"
+                                prepend-icon="mdi-account"
+                                label="用户名"
+                                type="text"
+                            />
+                            <v-text-field
+                                v-model="email"
+                                prepend-icon="mdi-email"
+                                label="注册邮箱"
+                                type="text"
+                                autocomplete="old-email"
+                            />
+                            <div
+                                align="center"
+                                class="mt-4"
                             >
-                                返回
-                            </v-btn>
-                            <v-btn
-                                rounded
-                                dark
-                                color="red"
-                                type="submit"
-                                :loading="loading"
-                            >
-                                重置密码
-                            </v-btn>
-                        </div>
-                    </v-form>
-                </v-card-text>
-                <v-alert
-                    v-if="alert.msg"
-                    :type="alert.type"
-                    class="ma-2"
-                >
-                    {{ alert.msg }}
-                </v-alert>
-            </v-card>
-        </v-col>
-    </v-row>
+                                <v-btn
+                                    rounded
+                                    color=""
+                                    class="mr-5"
+                                    @click="show_login = !show_login"
+                                >
+                                    返回
+                                </v-btn>
+                                <v-btn
+                                    rounded
+                                    dark
+                                    color="red"
+                                    type="submit"
+                                    :loading="loading"
+                                >
+                                    重置密码
+                                </v-btn>
+                            </div>
+                        </v-form>
+                    </v-card-text>
+                    <v-alert
+                        v-if="alert.msg"
+                        :type="alert.type"
+                        class="ma-2"
+                    >
+                        {{ alert.msg }}
+                    </v-alert>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script setup>
@@ -166,10 +168,6 @@ const router = useRouter();
 const store = useMainStore();
 const { $backend } = useNuxtApp();
 
-definePageMeta({
-    layout: 'blank'
-});
-
 const username = ref('');
 const password = ref('');
 const email = ref('');
@@ -179,6 +177,10 @@ const alert = ref({
     type: 'error',
     msg: '',
 });
+
+// 控制是否显示导航栏（可作为开关使用）
+const showNavbar = true; // 后期可通过配置或环境变量控制
+store.setNavbar(showNavbar);
 
 // Fetch user info on mount/creation to check login status
 // Note: In Nuxt 3, useAsyncData is preferred for SSR, but client-side is fine for login check
@@ -264,7 +266,7 @@ useHead({
 </script>
 
 <style scoped>
-.fill-center {
-    margin-top: 6%;
+.login-container {
+    min-height: calc(100vh - 120px);
 }
 </style>
--- app/pages/signup.vue
+++ app/pages/signup.vue
@@ -1,109 +1,111 @@
 
 <template>
-    <v-row
-        justify="center"
-        class="fill-center"
-    >
-        <v-col
-            cols="12"
-            sm="8"
-            md="4"
+    <div class="signup-container">
+        <v-row
+            justify="center"
+            class="mt-8"
         >
-            <v-card class="elevation-12">
-                <v-toolbar
-                    dark
-                    color="primary"
-                >
-                    <v-toolbar-title>填写注册信息</v-toolbar-title>
-                    <v-spacer />
-                    <v-btn
-                        rounded
-                        color="success"
-                        variant="elevated"
-                        to="/login"
-                        class="text-white mr-4"
+            <v-col
+                cols="12"
+                sm="8"
+                md="4"
+            >
+                <v-card class="elevation-12">
+                    <v-toolbar
+                        dark
+                        color="primary"
                     >
-                        登录
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text>
-                    <v-form
-                        ref="form"
-                        @submit.prevent="signup"
-                    >
-                        <v-text-field
-                            v-model="username"
-                            required
-                            prepend-icon="mdi-account"
-                            label="用户名"
-                            type="text"
-                            autocomplete="new-username"
-                            :rules="[rules.user]"
-                        />
-                        <v-text-field
-                            v-model="password"
-                            required
-                            prepend-icon="mdi-lock"
-                            label="密码"
-                            type="password"
-                            autocomplete="new-password"
-                            :rules="[rules.pass]"
-                        />
-                        <v-text-field
-                            v-model="password2"
-                            required
-                            prepend-icon="mdi-lock"
-                            label="确认密码"
-                            type="password"
-                            autocomplete="new-password2"
-                            :rules="[validPwd]"
-                        />
-                        <v-text-field
-                            v-model="nickname"
-                            required
-                            prepend-icon="mdi-face-man"
-                            label="昵称"
-                            type="text"
-                            autocomplete="new-nickname"
-                            :rules="[rules.nick]"
-                        />
-                        <v-text-field
-                            v-model="email"
-                            required
-                            prepend-icon="mdi-email"
-                            label="Email"
-                            type="text"
-                            autocomplete="new-email"
-                            :rules="[rules.email]"
-                        />
-                        <div
-                            align="center"
-                            class="mt-4"
+                        <v-toolbar-title>填写注册信息</v-toolbar-title>
+                        <v-spacer />
+                        <v-btn
+                            rounded
+                            color="success"
+                            variant="elevated"
+                            to="/login"
+                            class="text-white mr-4"
                         >
-                            <v-btn
-                                dark
-                                large
-                                rounded
-                                color="red"
-                                type="submit"
-                                :loading="loading"
+                            登录
+                        </v-btn>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form
+                            ref="form"
+                            @submit.prevent="signup"
+                        >
+                            <v-text-field
+                                v-model="username"
+                                required
+                                prepend-icon="mdi-account"
+                                label="用户名"
+                                type="text"
+                                autocomplete="new-username"
+                                :rules="[rules.user]"
+                            />
+                            <v-text-field
+                                v-model="password"
+                                required
+                                prepend-icon="mdi-lock"
+                                label="密码"
+                                type="password"
+                                autocomplete="new-password"
+                                :rules="[rules.pass]"
+                            />
+                            <v-text-field
+                                v-model="password2"
+                                required
+                                prepend-icon="mdi-lock"
+                                label="确认密码"
+                                type="password"
+                                autocomplete="new-password2"
+                                :rules="[validPwd]"
+                            />
+                            <v-text-field
+                                v-model="nickname"
+                                required
+                                prepend-icon="mdi-face-man"
+                                label="昵称"
+                                type="text"
+                                autocomplete="new-nickname"
+                                :rules="[rules.nick]"
+                            />
+                            <v-text-field
+                                v-model="email"
+                                required
+                                prepend-icon="mdi-email"
+                                label="Email"
+                                type="text"
+                                autocomplete="new-email"
+                                :rules="[rules.email]"
+                            />
+                            <div
+                                align="center"
+                                class="mt-4"
                             >
-                                注册
-                            </v-btn>
-                        </div>
-                    </v-form>
-                </v-card-text>
+                                <v-btn
+                                    dark
+                                    large
+                                    rounded
+                                    color="red"
+                                    type="submit"
+                                    :loading="loading"
+                                >
+                                    注册
+                                </v-btn>
+                            </div>
+                        </v-form>
+                    </v-card-text>
 
-                <v-alert
-                    v-if="failmsg"
-                    type="error"
-                    class="ma-2"
-                >
-                    {{ failmsg }}
-                </v-alert>
-            </v-card>
-        </v-col>
-    </v-row>
+                    <v-alert
+                        v-if="failmsg"
+                        type="error"
+                        class="ma-2"
+                    >
+                        {{ failmsg }}
+                    </v-alert>
+                </v-card>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script setup>
@@ -115,10 +117,6 @@ const router = useRouter();
 const store = useMainStore();
 const { $backend } = useNuxtApp();
 
-definePageMeta({
-    layout: 'blank'
-});
-
 const form = ref(null);
 const username = ref('');
 const password = ref('');
@@ -127,6 +125,10 @@ const nickname = ref('');
 const email = ref('');
 const failmsg = ref('');
 const loading = ref(false);
+
+// 控制是否显示导航栏（可作为开关使用）
+const showNavbar = true; // 后期可通过配置或环境变量控制
+store.setNavbar(showNavbar);
 
 const rules = {
     user: v => ( v && 20 >= v.length && v.length >= 5) || '用户名长度必须在5-20个字符之间',
@@ -195,7 +197,7 @@ useHead({
 </script>
 
 <style scoped>
-.fill-center {
-    margin-top: 6%;
+.signup-container {
+    min-height: calc(100vh - 120px);
 }
 </style>
