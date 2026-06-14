9a6245a9aea0598589e35c41fa3a1bdd96a5c742
hehetoshang <shangzhen0831@163.com>
2026-06-14 20:48:53 +0800

    lint

--- webserver/plugins/meta/biquge/api.py
+++ webserver/plugins/meta/biquge/api.py
@@ -23,9 +23,7 @@ CHROME_HEADERS = {
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.6",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/131.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
     ),
 }
 TIMEOUT = 15
